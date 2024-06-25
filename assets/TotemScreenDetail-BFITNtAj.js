5d97d6293ccc4acd388d5bbecbb4e6fbfdb77bcd
João Apostulo Neto <japostulo@gmail.com>
2024-06-25 20:57:14 -0300

    teste

--- assets/TotemScreenDetail-BFITNtAj.js
+++
@@ -1,1 +0,0 @@
-import{ae as e}from"./index-eSwy3xtz.js";const t={};function n(r,c){return" Tela em desenvolvimento... "}const a=e(t,[["render",n]]);export{a as default};
---
+++ assets/TotemScreenDetail-CfHxNvvl.js
@@ -0,0 +1,1 @@
+import{ae as e}from"./index-_dW35LJ9.js";const t={};function n(r,c){return" Tela em desenvolvimento... "}const a=e(t,[["render",n]]);export{a as default};
