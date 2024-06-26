6359fe4e0b2f55a91a76e2ceb61c758669a150e3
João Apostulo Neto <japostulo@gmail.com>
2024-06-26 20:47:00 -0300

    build

---
+++ assets/Queue-DpIzcum_.js
@@ -0,0 +1,1 @@
+import{ae as o,a1 as r,ai as t,a0 as c}from"./index-DXKN8KeX.js";const n={};function a(s,_){const e=t("router-view");return c(),r(e)}const f=o(n,[["render",a]]);export{f as default};
---
+++ assets/TotemScreenDetail-CrGgfWEv.js
@@ -0,0 +1,1 @@
+import{ae as e}from"./index-DXKN8KeX.js";const t={};function n(r,c){return" Tela em desenvolvimento... "}const a=e(t,[["render",n]]);export{a as default};
